fcd5abf44aa8677e0ac7d8bc6e8df854c31d7934
imr4nn <218129649+syedimranhere@users.noreply.github.com>
2025-08-03 04:48:00 +0500

    cookie issue fixed

--- src/middleware/getemail.js
+++ src/middleware/getemail.js
@@ -3,9 +3,11 @@ export const extractResetEmail = (req, res, next) => {
   const email = req.cookies?.resetEmail;
   console.log("cookies email check");
   if (!email) {
-    return res
-      .status(401)
-      .json({ success: false, message: "Session expired. Restart flow." });
+    return res.status(401).json({
+      em: email,
+      success: false,
+      message: "Session expired. Restart flow.",
+    });
   }
   req.resetEmail = email; // Attach to request
   next();
